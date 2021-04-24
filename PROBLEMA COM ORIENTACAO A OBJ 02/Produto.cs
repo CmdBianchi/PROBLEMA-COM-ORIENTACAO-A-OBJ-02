e81f4e28f6cf4e07475dc860355f9420a5ff3391
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-04-24 16:10:11 -0300

    Adicionar arquivos de projeto.

---
+++ PROBLEMA COM ORIENTACAO A OBJ 02/Produto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PROBLEMA_COM_ORIENTACAO_A_OBJ_02{
+    //----------------------- OBJ PRODUTO -----------------------//
+    class Produto{ //----> Atributos da classe
+        public string Nome;
+        public double Preco;
+        public int Quantidade;        
+        //--------------------------------------------//
+        public double ValorTotalEmEstoque(){//---> Métodos da classe
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade){
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade){
+            Quantidade -= quantidade;
+        }
+        public override string ToString(){
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2")
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2");
+        }
+        //--------------------------------------------//
+        //--->END
+    }
+}
